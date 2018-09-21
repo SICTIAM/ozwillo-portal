43a2486a0da399b11115c8856f0415d2aa3c70e1
Arnaud Zago <arnaud.zago@gmail.com>
2018-09-21 09:50:56 +0200

    [DEV] remove unnecessary code

--- src/main/resources/public/js/components/redirect-button-with-tooltip.js
+++ src/main/resources/public/js/components/redirect-button-with-tooltip.js
@@ -1,28 +1,18 @@
 import * as React from "react";
 import CustomTooltip from "./custom-tooltip";
+import {Link} from "react-router-dom";
 import PropTypes from "prop-types";
-import Redirect from "react-router/es/Redirect";
 
 export default class RedirectButtonWithTooltip extends React.PureComponent {
 
-    state = {
-      isClicked: false
-    };
 
     render() {
         const {tooltipTitle, link, children, ...rest} = this.props;
-        const {isClicked} = this.state;
-        let redirect = null;
-        if(isClicked){
-            redirect = <Redirect to={link}/>;
-        }
-
         return (
             <CustomTooltip title={tooltipTitle}>
-                {redirect}
-                <div {...rest} onClick={() => this.setState({isClicked: true})}>
+                <Link to={link} {...rest}>
                     {children}
-                </div>
+                </Link>
             </CustomTooltip>
         )
     }
