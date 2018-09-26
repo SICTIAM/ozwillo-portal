3fc8f0243b161639b6c94de8a048247c55c653a4
Arnaud Zago <arnaud.zago@gmail.com>
2018-09-26 17:09:57 +0200

    [DEV] SearchBar designed and connected

--- src/main/resources/public/js/components/app.js
+++ src/main/resources/public/js/components/app.js
@@ -1,11 +1,18 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import AppModal from "./store-install-app";
 
 export default class App extends React.PureComponent{
 
+    openApp = () =>{
+        this.modal.open();
+    };
+
     render = () => {
-        const indicatorStatus = this.props.app.installed ? "installed" : (this.props.app.paid ? "paid" : "free");
-        const pubServiceIndicator = this.props.app.public_service ?
+        const {app} = this.props;
+
+        const indicatorStatus = app.installed ? "installed" : (app.paid ? "paid" : "free");
+        const pubServiceIndicator = app.public_service ?
             <div className="public-service-indicator">
                 <div className="triangle"/>
                 <div className="label">
@@ -15,18 +22,20 @@ export default class App extends React.PureComponent{
 
         return (
             <div className="col-lg-2 col-md-3 col-sm-4 col-xs-6 container-app">
+                <AppModal app={this.props.app} wrappedComponentRef={
+                    c => { this.modal = c && c.getWrappedInstance() }}/>
                 <div className="app">
                     <div className="logo">
-                        <img src={this.props.app.icon}/>
+                        <img src={app.icon}/>
                     </div>
-                    <div className="description" onClick={() => console.log("open")}>
+                    <div className="description" onClick={() => this.openApp()}>
                         {pubServiceIndicator}
                         <div className="app-header">
-                            <span className="app-name">{this.props.app.name}</span>
-                            <p className="app-provider">{this.props.app.provider}</p>
+                            <span className="app-name">{app.name}</span>
+                            <p className="app-provider">{app.provider}</p>
                         </div>
-                        <p className="app-description">{this.props.app.description}</p>
-                        {/*<Indicator status={indicatorStatus}/>*/}
+                        <p className="app-description">{app.description}</p>
+                        <Indicator status={indicatorStatus}/>
                     </div>
                 </div>
             </div>
@@ -36,4 +45,53 @@ export default class App extends React.PureComponent{
 
 App.propTypes = {
     app: PropTypes.object
+};
+
+
+
+export class Indicator extends React.PureComponent {
+    render() {
+        let btns;
+        const status = this.props.status;
+        if (status === "installed") {
+            btns = [
+                <button type="button" key="indicator_button"
+                        className="btn btn-lg btn-installed">{this.context.t('installed')}</button>,
+                <button type="button" key="indicator_icon" className="btn btn-lg btn-installed-indicator">
+                    <i className="fa fa-check" />
+                </button>
+            ];
+        } else if (status === "free") {
+            btns = [
+                <button type="button" key="indicator_button"
+                        className="btn btn-lg btn-free">{this.context.t('free')}</button>,
+                <button type="button" key="indicator_icon" className="btn btn-lg btn-free-indicator">
+                    <i className="fa fa-gift" />
+                </button>
+            ];
+        } else {
+            btns = [
+                <button type="button" key="indicator_button"
+                        className="btn btn-lg btn-buy">{this.context.t('paid')}</button>,
+                <button type="button" key="indicator_icon" className="btn btn-lg btn-buy-indicator">
+                    <i className="fa fas fa-euro-sign" />
+                </button>
+            ];
+        }
+
+        return (
+            <div className="app-status text-center">
+                {btns}
+            </div>
+        );
+    }
+}
+
+Indicator.propTypes = {
+  status: PropTypes.string
+};
+
+
+Indicator.contextTypes = {
+    t: PropTypes.func.isRequired
 };
