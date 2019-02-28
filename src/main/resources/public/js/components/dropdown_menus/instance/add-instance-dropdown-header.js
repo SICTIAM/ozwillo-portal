c418fb0c3196ee102ad5df7bcacd51373f37dd19
zago <arnaud.zago@gmail.com>
2019-02-28 16:33:17 +0100

    feat: display all apps which could be installed when scrolling the select component

--- src/main/resources/public/js/components/dropdown_menus/instance/add-instance-dropdown-header.js
+++ src/main/resources/public/js/components/dropdown_menus/instance/add-instance-dropdown-header.js
@@ -3,15 +3,19 @@ import PropTypes from 'prop-types';
 import Select from 'react-select';
 import { i18n } from "../../../config/i18n-config"
 import NotificationMessageBlock from '../../notification-message-block';
+import customFetch from '../../../util/custom-fetch';
+import Config from '../../../config/config';
+
+const AppTypes = Config.appTypes;
+const MIN_APPS_NEEDED_TO_SCROLL = 7;
 
 class AddInstanceDropdownHeader extends React.Component {
 
-
     static propTypes = {
-        apps: PropTypes.array.isRequired,
         app: PropTypes.object,
         onAddInstance: PropTypes.func.isRequired,
-        onChangeInstance: PropTypes.func.isRequired
+        onChangeInstance: PropTypes.func.isRequired,
+        organization: PropTypes.object
     };
 
     constructor(props) {
@@ -19,12 +23,19 @@ class AddInstanceDropdownHeader extends React.Component {
 
         this.state = {
             error: {message: '', http_status: 200},
-            isLoading: false
+            isLoading: false,
+            apps: [],
+            maybeMoreApps: false,
+            totalAppsFetched: 0
         };
 
         //bind methods
         this.onOptionChange = this.onOptionChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    componentDidMount = async () =>  {
+        await this._fetchApplications();
     }
 
     onOptionChange(selectedOption) {
@@ -53,19 +64,68 @@ class AddInstanceDropdownHeader extends React.Component {
             });
     }
 
+    filterApps = (app) => {
+        const org = this.props.organization;
+        // only applications
+        if (AppTypes.application !== app.type) {
+            return false;
+        }
+
+        //Check types
+        return (app.target_publicbodies && org.type === 'PUBLIC_BODY') ||
+            (app.target_companies && org.type === 'COMPANY') ||
+            (app.target_citizens && !org.type);
+    }
+
+    _fetchApplications = async () => {
+        const {apps, maybeMoreApps, totalAppsFetched} = this.state
+        const {apps: appsFetched, maybeMoreApps: maybeMoreAppsFetched}  = await customFetch(`/api/store/applications`, {urlParams: {
+                target_citizens: true,
+                target_publicbodies: true,
+                target_companies: true,
+                free: true,
+                paid: true,
+                last: totalAppsFetched
+            }});
+        let appsRes = appsFetched.filter(app => this.filterApps(app)).concat(apps)
+        this.setState({apps: appsRes , maybeMoreApps: maybeMoreAppsFetched, totalAppsFetched: totalAppsFetched + appsFetched.length},async ()  => {
+            if(appsRes.length < MIN_APPS_NEEDED_TO_SCROLL && maybeMoreAppsFetched){
+                await this._fetchApplications();
+            }
+        });
+
+        return {apps: apps, maybeMoreApps: maybeMoreApps}
+    }
+
+    _handleMoreApplications = async () => {
+        const {totalAppsFetched, apps} =  this.state
+        const {apps: appsFetched, maybeMoreApps} = await customFetch(`/api/store/applications`, {urlParams: {
+                target_citizens: true,
+                target_publicbodies: true,
+                target_companies: true,
+                free: true,
+                paid: true,
+                last: totalAppsFetched
+        }});
+        this.setState({apps: appsFetched.concat(apps), totalAppsFetched: totalAppsFetched + appsFetched.length, maybeMoreApps: maybeMoreApps})
+    };
+
     render() {
-        let {error} = this.state;
+        let {error, apps} = this.state;
         return <header className="dropdown-header">
             <form className="form flex-row" onSubmit={this.onSubmit}>
                 <Select
+                    valueKey={'id'}
                     className="select add-instance-select"
                     name="app"
                     value={this.props.app}
                     labelKey="name"
                     onChange={this.onOptionChange}
-                    options={this.props.apps}
+                    options={apps}
                     placeholder={i18n._('organization.desc.applications')}
-                    required={true}/>
+                    required={true}
+                    onMenuScrollToBottom={this._handleMoreApplications}
+                />
 
                 <div className="options flex-row">
                     {
@@ -90,5 +150,16 @@ class AddInstanceDropdownHeader extends React.Component {
         </header>;
     }
 }
+
+const styles = {
+    displayMore :{
+        position: "absolute",
+        height: "50%",
+        width: "15%",
+        display: "flex",
+        alignItems:"flex-end",
+        justifyContent: "center"
+    }
+};
 
 export default AddInstanceDropdownHeader;
